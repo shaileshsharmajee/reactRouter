3526cdc283d4ec9b41dfb501b70a8a7245f1a767
Shailesh Sharma <vinodw51822@gmail.com>
2025-07-19 19:07:20 +0530

    context api

--- contextApi/src/App.jsx
+++ contextApi/src/App.jsx
@@ -1,13 +1,17 @@
-
 import './App.css'
+import Login from './Components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './Context/UserContextProvider'
 
 function App() {
-
+  
 
   return (
-    < UserContextProvider>
-    <h1>react jai hum </h1>
-    </ UserContextProvider>
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
--- contextApi/src/Context/UserContextProvider.jsx
+++ contextApi/src/Context/UserContextProvider.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider=({childern})=>{
-    const [user,setUser]=useState(null)
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
     return(
-        <UserContext.Provider value={{user,setUser}}>
-        {childern}
-        </ UserContext.Provider>
-    );
-
+        <UserContext.Provider value={{user, setUser}}>
+        {children}
+        </UserContext.Provider>
+    )
 }
 
-export default UserContextProvider;
+export default UserContextProvider
